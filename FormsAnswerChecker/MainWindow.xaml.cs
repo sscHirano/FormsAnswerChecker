a8fb4f8c4ef1f0a83e9004b8972d59401fd8a85e
修一 平野 <shuichi_hirano@solver.co.jp>
2021-07-31 14:22:34 +0900

    ファイルのドラッグに対応

--- FormsAnswerChecker/MainWindow.xaml.cs
+++ FormsAnswerChecker/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FormsAnswerChecker
 {
@@ -23,6 +11,18 @@ namespace FormsAnswerChecker
         public MainWindow()
         {
             InitializeComponent();
+
+            AddHandler(TextBox.DropEvent, new DragEventHandler(FileListBox_Drop), true);
         }
+
+        private void FileListBox_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+                
+            }
+        }
+
     }
 }
